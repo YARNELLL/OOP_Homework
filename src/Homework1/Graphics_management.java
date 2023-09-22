b994b48e934a63265d195a3250b1b529553f87e8
YARNELLL <59241267+YARNELLL@users.noreply.github.com>
2023-09-22 20:28:13 +0800

    9月22号20.27修改代码

--- src/Homework1/Graphics_management.java
+++ src/Homework1/Graphics_management.java
@@ -1,22 +1,64 @@
 package Homework1;
 
 import java.lang.Math;
-
+import java.util.Scanner;
+/**
+ * @author 吴禹 2023214309
+ */
 public class Graphics_management {
     public static void main(String[] args){
-
+        Scanner sc = new Scanner(System.in);
+        Graphics_Factory graphicsFactory = new Graphics_Factory();
+        String graphicsType = null;
+        System.out.println("选择三种图形中的一种，椭圆形、矩形和三角形：");
+        graphicsType = sc.next();
+        Graphics graphics = graphicsFactory.getGraphics(graphicsType);
+        if(graphics != null){
+            graphics.draw();
+            System.out.format("%s的面积为：%.2f\n", graphicsType ,graphics.getArea());
+            System.out.format("%s的周长为：%.2f\n", graphicsType, graphics.getPerimeter());
+        }
+        else{
+            System.out.println("请输入有效的图形。");
+        }
+        sc.close();
     }
 }
 
-//椭圆形
-class Ellipsoid implements Graphics{
-    double major_semi_axis;
-    double minor_semi_axis;
 
-    public Ellipsoid(double major_semi_axis, double minor_semi_axis){
+class Ellipsoid implements Graphics{
+    /*
+    椭圆形
+     */
+    private double major_semi_axis;
+    private double minor_semi_axis;
+
+    public double getMajor_semi_axis() {
+        return major_semi_axis;
+    }
+
+    public void setMajor_semi_axis(double major_semi_axis) {
+        this.major_semi_axis = major_semi_axis;
+    }
+
+    public double getMinor_semi_axis() {
+        return minor_semi_axis;
+    }
+
+    public void setMinor_semi_axis(double minor_semi_axis) {
+        this.minor_semi_axis = minor_semi_axis;
+    }
+
+    public Ellipsoid(double major_semi_axis, double minor_semi_axis) {
         this.major_semi_axis = major_semi_axis;
         this.minor_semi_axis = minor_semi_axis;
     }
+
+    @Override
+    public void draw() {
+        System.out.println("画了一个椭圆形。");
+    }
+
     @Override
     public double getArea() {
         return Math.PI * major_semi_axis * minor_semi_axis;
@@ -24,19 +66,43 @@ class Ellipsoid implements Graphics{
 
     @Override
     public double getPerimeter() {
-        return 2 * Math.PI * minor_semi_axis + 4 * (major_semi_axis - minor_semi_axis);
+        return 2 * Math.PI * minor_semi_axis +
+                4 * (major_semi_axis - minor_semi_axis);
     }
 }
-
-//矩形
 class Rectangle implements Graphics{
-    double length;
-    double width;
+    /*
+    矩形
+     */
+    private double length;
+    private double width;
 
-    public Rectangle(double length, double width){
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public Rectangle(double length, double width) {
         this.length = length;
         this.width = width;
     }
+
+    @Override
+    public void draw() {
+        System.out.println("画了一个矩形");
+    }
+
     @Override
     public double getArea() {
         return length * width;
@@ -48,16 +114,47 @@ class Rectangle implements Graphics{
     }
 }
 
-//三角形
 class Triangle implements Graphics{
-    double edge_1;
-    double edge_2;
-    double edge_3;
+    /*
+    三角形
+     */
+    private double edge_1;
+    private double edge_2;
+    private double edge_3;
+
+    public double getEdge_1() {
+        return edge_1;
+    }
+
+    public void setEdge_1(double edge_1) {
+        this.edge_1 = edge_1;
+    }
+
+    public double getEdge_2() {
+        return edge_2;
+    }
+
+    public void setEdge_2(double edge_2) {
+        this.edge_2 = edge_2;
+    }
+
+    public double getEdge_3() {
+        return edge_3;
+    }
+
+    public void setEdge_3(double edge_3) {
+        this.edge_3 = edge_3;
+    }
 
     public Triangle(double edge_1, double edge_2, double edge_3) {
         this.edge_1 = edge_1;
         this.edge_2 = edge_2;
         this.edge_3 = edge_3;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("画了一个三角形");
     }
 
     @Override
@@ -68,12 +165,57 @@ class Triangle implements Graphics{
 
     @Override
     public double getPerimeter() {
-        return 0;
+        return edge_1 + edge_2 + edge_3;
     }
 }
 
 interface Graphics{
+    /*
+    图形接口，存放图形类所需的函数
+     */
+    void draw();
     double getArea();
-
     double getPerimeter();
+}
+
+class Graphics_Factory{
+    public Graphics getGraphics(String graphicsType){
+        Scanner sc = new Scanner(System.in);
+        Graphics graphics = null;
+        switch (graphicsType) {
+            case "椭圆形":
+                double major_semi_axis;
+                double minor_semi_axis;
+                System.out.println("输入椭圆形的长半轴和短半轴，中间用空格隔开：");
+                major_semi_axis = sc.nextDouble();
+                minor_semi_axis = sc.nextDouble();
+                if(major_semi_axis >= minor_semi_axis){
+                    graphics = new Ellipsoid(major_semi_axis, minor_semi_axis);
+                }
+                else{
+                    System.out.println("长半轴应该比短半轴长。");
+                }
+                break;
+            case "矩形":
+                double length;
+                double width;
+                System.out.println("输入矩形的长和宽，中间用空格隔开：");
+                length = sc.nextDouble();
+                width = sc.nextDouble();
+                graphics = new Rectangle(length, width);
+                break;
+            case "三角形":
+                double edge_1;
+                double edge_2;
+                double edge_3;
+                System.out.println("输入三角形的三条边，中间用空格隔开：");
+                edge_1 = sc.nextDouble();
+                edge_2 = sc.nextDouble();
+                edge_3 = sc.nextDouble();
+                graphics = new Triangle(edge_1, edge_2, edge_3);
+                break;
+        }
+        sc.close();
+        return graphics;
+    }
 }
--- src/Homework1/Numeric_conversion.java
+++ src/Homework1/Numeric_conversion.java
@@ -1,5 +1,7 @@
 package Homework1;
 
 public class Numeric_conversion {
+    public static void main(String[] args){
 
+    }
 }
