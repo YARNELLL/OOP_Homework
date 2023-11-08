75e371330443ad7eb6185b59b3535265c3394868
YARNELLL <59241267+YARNELLL@users.noreply.github.com>
2023-11-08 17:49:22 +0800

    11月8号17：49电脑2修改

---
+++ src/Homework7/part1/RecruitmentSystem.java
@@ -0,0 +1,90 @@
+package Homework7.part1;
+
+import java.util.*;
+
+public class RecruitmentSystem {
+    public static void main(String[] args){
+        JobSeeker s1 = new JobSeeker("s1");
+        JobSeeker s2 = new JobSeeker("s2");
+        JobSeeker s3 = new JobSeeker("s3");
+        JobSeeker s4 = new JobSeeker("s4");
+        JobSeeker s5 = new JobSeeker("s5");
+
+        Job j1 = new Job("前端","北京");
+        Job j2 = new Job("后端","北京");
+        Job j3 = new Job("大数据","北京");
+        Job j4 = new Job("算法","北京");
+        Job j5 = new Job("后端","上海");
+        Job j6 = new Job("前端","深圳");
+
+        j1.add_company("c1");
+        j1.add_subscriber(s1);
+        j1.add_company("c2");
+        j1.add_subscriber(s2);
+        j1.add_company("c3");
+        j2.add_subscriber(s3);
+        j2.add_subscriber(s4);
+        j2.add_subscriber(s5);
+        j2.add_company("c4");
+    }
+}
+
+interface Subscriber{
+    void update(String company_name);
+}
+
+class JobSeeker implements Subscriber{
+    private String name;
+    private List<String> companies;
+
+    public JobSeeker(String name) {
+        this.name = name;
+    }
+
+    public void setCompanies(List<String> companies) {
+        this.companies = new ArrayList<>(companies);
+
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    @Override
+    public void update(String company_name) {
+        this.companies.add(company_name);
+        System.out.println(this.name + "收到通知：" + company_name + "发布了符合订阅的工作岗位。");
+    }
+}
+
+class Job{
+    private List<JobSeeker> subscribers;
+    private List<String> companies;
+    private String job_type;
+    private String workplace;
+
+    public Job(String job_type, String workplace) {
+        this.subscribers = new ArrayList<>();
+        this.companies = new ArrayList<>();
+        this.job_type = job_type;
+        this.workplace = workplace;
+    }
+
+    public void add_subscriber(JobSeeker subscriber){
+        this.subscribers.add(subscriber);
+        System.out.println("求职者" +
+                subscriber.getName() + "订阅了工作类型：" + this.job_type + "，工作地点：" + this.workplace);
+        subscriber.setCompanies(this.companies);
+        System.out.println("符合订阅工作的公司有：");
+        for(String company : companies){
+            System.out.println(company);
+        }
+    }
+
+    public void add_company(String company_name){
+        this.companies.add(company_name);
+        for(JobSeeker subscriber : this.subscribers){
+            subscriber.update(company_name);
+        }
+    }
+}
